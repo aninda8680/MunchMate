5cd60db26fae32f9acf7827601632b4c7342b541
Atanu2k4 <atanu.saha2004@gmail.com>
2025-04-11 23:35:47 +0530

    Refactor Navbar component for improved readability and maintainability; add navigation functionality to OrderNowButton

--- src/components/OrderNowButton.jsx
+++ src/components/OrderNowButton.jsx
@@ -1,8 +1,18 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const OrderNowButton = () => {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate("/menu");
+  };
+
   return (
-    <button className="relative border hover:border-orange-600 duration-500 group cursor-pointer text-orange-50 overflow-hidden h-14 w-56 rounded-md bg-orange-800 p-2 flex justify-center items-center font-extrabold">
+    <button
+      onClick={handleClick}
+      className="relative border hover:border-orange-600 duration-500 group cursor-pointer text-orange-50 overflow-hidden h-14 w-56 rounded-md bg-orange-800 p-2 flex justify-center items-center font-extrabold"
+    >
       <div className="absolute z-10 w-48 h-48 rounded-full group-hover:scale-150 transition-all duration-500 ease-in-out bg-orange-900 delay-150 group-hover:delay-75" />
       <div className="absolute z-10 w-40 h-40 rounded-full group-hover:scale-150 transition-all duration-500 ease-in-out bg-orange-800 delay-150 group-hover:delay-100" />
       <div className="absolute z-10 w-32 h-32 rounded-full group-hover:scale-150 transition-all duration-500 ease-in-out bg-orange-700 delay-150 group-hover:delay-150" />
